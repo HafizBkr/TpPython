cbd0695993170426c03cb6e4ee8169d77a8fdd57
HafizBkr <hafizinovus@gmail.com>
2025-04-05 22:15:17 +0000

    commit pour l'exercice 6

---
+++ tp1/numero6.py
@@ -0,0 +1,11 @@
+# affichage de chaque lettre d'un mot en utilisant for
+mot = input("Entrez un mot : ")
+for lettre in mot:
+    print(lettre)
+
+# affichage de chaque element d'une liste avec une boucle for
+
+print("Affichage de chaque élément de la liste :")
+liste=["hello","world","python",12,3.14]
+for element in liste:
+    print(element)
