d05c551dab416569fe5929b71c2fc7ffc913037c
HafizBkr <hafizinovus@gmail.com>
2025-04-05 22:00:12 +0000

    commit for exercice 4

--- tp1/numero4.py
+++ tp1/numero4.py
@@ -1,1 +1,14 @@
-#exercie 
+#exercie concernant a et b
+a=0
+b=10
+
+print("Premiere boucle\n")
+while a < b:
+     print(f"a={a}")
+     a+=1
+
+print("deuxiem boucle\n")
+while b!=0:
+  b-=1
+  if b % 2 != 0:
+    print(f"b={b}")
