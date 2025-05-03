d04fd7de90bce49143026c061c2d0125ab0f66d5
Hafiz BOUKARI <114757610+HafizBkr@users.noreply.github.com>
2025-05-03 23:44:52 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,1 @@
-package main
 
-import (
-	"fmt"
-)
-
-func main() {
-	tableau := []string{"hello", "kevine", "jaques"}
-
-	fmt.Println("===Deuxieme boucle===")
-	for i := 0; i <= 2; i++ {
-		fmt.Println(i, tableau[i])
-	}
-
-}
